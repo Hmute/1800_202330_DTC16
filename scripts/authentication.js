4c8fe6f493292c813105a21e4cb740a8a4934707
Hmute <heraldoabreusound@gmail.com>
2023-11-05 14:01:12 -0800

    minimal changes to the files

--- scripts/authentication.js
+++ scripts/authentication.js
@@ -1,3 +1,4 @@
+// Initialize the FirebaseUI Widget using Firebase.
 var ui = new firebaseui.auth.AuthUI(firebase.auth());
 
 var uiConfig = {
@@ -21,7 +22,7 @@ var uiConfig = {
         // Leave the lines as is for the providers you want to offer your users.
         // firebase.auth.GoogleAuthProvider.PROVIDER_ID,
         // firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-        //firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+        // firebase.auth.TwitterAuthProvider.PROVIDER_ID,
         // firebase.auth.GithubAuthProvider.PROVIDER_ID,
         firebase.auth.EmailAuthProvider.PROVIDER_ID,
         // firebase.auth.PhoneAuthProvider.PROVIDER_ID
