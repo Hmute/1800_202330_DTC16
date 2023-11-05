022aeaf9406b587fe323b37f608f74d42c464c01
Richard Maceda <richardmaceda204@gmail.com>
2023-11-04 21:19:27 -0700

    initial commit of Side Nav Bar Loader

---
+++ scripts/sideNavBarLoader.js
@@ -0,0 +1,15 @@
+$(function () {
+    $(".sideNavBarDiv").load("sideNavBar.html");
+});
+
+function eventSideNavBar() {
+    const sidebar = document.getElementById("side_nav");
+    if (sidebar.style.display === "none") {
+        sidebar.style.display = "block";
+        // transition for sidebar
+        // sidebar.style.transition = "width 2s";
+    } else {
+        sidebar.style.display = "none";
+    }
+
+}
