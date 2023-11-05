610a4f3d6877efcec5d79d1d7ec06fc246aa9520
Richard Maceda <richardmaceda204@gmail.com>
2023-11-04 21:45:08 -0700

    made sideNavBar display to none

--- scripts/sideNavBarScript.js
+++ scripts/sideNavBarScript.js
@@ -1,5 +1,6 @@
 function eventSideNavBar() {
     const sidebar = document.getElementById("side_nav");
+    console.log(sidebar);
     if (sidebar.style.display === "none") {
         sidebar.style.display = "block";
         // transition for sidebar
