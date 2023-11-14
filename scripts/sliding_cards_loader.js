25235b40d5f34fde1957b5d1b46a92ea3e02f04a
Richard Maceda <richardmaceda204@gmail.com>
2023-11-13 17:47:43 -0800

    change html file of swiping cards

--- scripts/sliding_cards_loader.js
+++ scripts/sliding_cards_loader.js
@@ -24,40 +24,72 @@ const db = firebase.firestore();
 
 $(function () {
     $(".slidingCardsDiv").load("slidingCards.html");
-});
+});                        
 
 // Path: scripts/sliding_cards.js
 
-function makeCards(collectionId) {
-    let cardTemplate = document.querySelector("#individual_card");
-    let cardContainer = document.querySelector(".card-container");
+// function displayCardsDynamically(collection) {
+//     let cardTemplate = document.getElementById("swipeCardTemplate"); // Retrieve the HTML element with the ID "hikeCardTemplate" and store it in the cardTemplate variable. 
 
-    db.collection(collectionId).get().then(documentData => {
-        documentData.forEach(docData => {
-            var city = docData.data().city;
-            var date = docData.data().date;
-            var information = docData.data().information;
-            var parking = docData.data().parking;
-            var sports = docData.data().sports;
+//     db.collection(collection).get()   //the collection called "hikes"
+//         .then(allHikes => {
+//             //var i = 1;  //Optional: if you want to have a unique ID for each hike
+//             allHikes.forEach(doc => { //iterate thru each doc
+//                 var title = doc.data().name;       // get value of the "name" key
+//                 var details = doc.data().details;  // get value of the "details" key
+//                 var hikeCode = doc.data().code;    //get unique ID to each hike to be used for fetching right image
+//                 var hikeLength = doc.data().length; //gets the length field
+//                 var docID = doc.id;
+//                 let newcard = cardTemplate.content.cloneNode(true); // Clone the HTML template to create a new card (newcard) that will be filled with Firestore data.
 
-            let newCard = cardTemplate.cloneNode(true);
+//                 //update title and text and image
+//                 newcard.querySelector('.card-title').innerHTML = title;
+//                 newcard.querySelector('.card-length').innerHTML = hikeLength + "km";
+//                 newcard.querySelector('.card-text').innerHTML = details;
+//                 newcard.querySelector('.card-image').src = `/images/${hikeCode}.jpg`; //Example: NV01.jpg
+//                 newcard.querySelector('a').href = "eachHike.html?docID=" + docID;
 
-            newCard.querySelector("#Title").innerHTML = sports;
-            newCard.querySelector("#Information").innerHTML = information;
-            newCard.querySelector("#Parking").innerHTML = parking;
-            newCard.querySelector("#Sport").innerHTML = sports;
-            newCard.querySelector("#Date").innerHTML = date;
+//                 //Optional: give unique ids to all elements for future use
+//                 // newcard.querySelector('.card-title').setAttribute("id", "ctitle" + i);
+//                 // newcard.querySelector('.card-text').setAttribute("id", "ctext" + i);
+//                 // newcard.querySelector('.card-image').setAttribute("id", "cimage" + i);
 
-            cardContainer.appendChild(newCard);
+//                 //attach to gallery, Example: "hikes-go-here"
+//                 document.getElementById(collection + "-go-here").appendChild(newcard);
 
-            // Debugging: Log the retrieved data
-            console.log("Title:", sports);
-            console.log("Information:", information);
-            console.log("Parking:", parking);
-            console.log("Sport:", sports);
-            console.log("Date:", date);
-        });
-    });
+//                 //i++;   //Optional: iterate variable to serve as unique ID
+//             })
+//         })
+// }
+
+// displayCardsDynamically("hikes");  //input param is the name of the collection
+
+function insertNameFromFirestore() {
+    // Check if the user is logged in:
+    firebase.auth().onAuthStateChanged(user => {
+        if (user) {
+
+            console.log(user.uid); // Let's know who the logged-in user is by logging their UID
+
+            currentUser = db.collection("users").doc(user.uid); // Go to the Firestore document of the user
+
+            currentUser.get().then(userDoc => {
+                // Get the user name
+                var userName = userDoc.data().name;
+
+
+
+                console.log(userName); //just printing  in the database 
+                
+                
+                
+                //$("#name-goes-here").text(userName); // jQuery
+                document.getElementById("name-goes-here").innerText = userName;
+            })
+        } else {
+            console.log("No user is logged in."); // Log a message when no user is logged in
+        }
+    })
 }
 
-makeCards("Events");
+insertNameFromFirestore();
