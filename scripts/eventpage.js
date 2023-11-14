c6128c8092bb0cb30dd4f8e30e80977a955ec5b1
Marc Angelo Arnaldo <arnaldomarcangelo@gmail.com>
2023-11-14 13:11:13 -0800

    change how favorites in saved per user

--- scripts/eventpage.js
+++ scripts/eventpage.js
@@ -93,6 +93,7 @@ function createEventCard(collectionId) {
         });
     });
 }
+createEventCard("Events")
 
 
 function createGymCard(collectionId) {
@@ -103,6 +104,7 @@ function createGymCard(collectionId) {
             var title = docData.data().Gym_name;
             var rating = docData.data().Rating;
             var information = docData.data().Information;
+            var docId = docData.id
             let newCard;
 
             if (counter === 0) {
@@ -117,13 +119,11 @@ function createGymCard(collectionId) {
 
             document.body.appendChild(newCard);
             counter++;
+
+
         });
     });
 }
-
-createEventCard("Events")
-
-
 createGymCard("Gyms")
 
 document.addEventListener("click", function (event) {
@@ -133,43 +133,60 @@ document.addEventListener("click", function (event) {
         // The heart is currently empty, so fill it
         heartIcon.classList.remove("far");
         heartIcon.classList.add("fas");
-    
+
         const eventCard = heartIcon.closest(".eventcard[data-document-id]");
-        if(eventCard){
+        if (eventCard) {
             const user = firebase.auth().currentUser;
 
-            var documentId = eventCard.getAttribute("data-document-id")
-            db.collection("users").get().then(userDocs =>{
-                userDocs.forEach(userDocsData =>{
-                    if(userDocsData.data().name == user.displayName){
-                        db.collection("Events").doc(documentId).get().then(docData =>{
-                            var userFavorites = db.collection("users").doc(userDocsData.id).collection("Favorites")
+            var documentId = eventCard.getAttribute("data-document-id");
 
-                            userFavorites.add({
-                                sport: docData.data().sport,
-                                date: docData.data().date,
-                                information: docData.data().information,
-                                title: docData.data().title,
-                                last_updated: firebase.firestore.FieldValue.serverTimestamp()  //current system time
-                            })
-                        })
-                    }
-                })
-            })
+            userDoc = db.collection("users").doc(user.uid);
 
+            if (user) {
+                const userDoc = db.collection("users").doc(user.uid);
+
+                // Update the user's document with the favorite event
+                userDoc.update({
+                    favorites: firebase.firestore.FieldValue.arrayUnion(documentId)
+                }).then(() => {
+                    console.log("Event added to favorites");
+                }).catch(error => {
+                    console.error("Error adding event to favorites:", error);
+                });
+            }
         }
-
-    }else{
+    } else {
+        // The heart is currently filled, so unfill it
         heartIcon.classList.remove("fas");
         heartIcon.classList.add("far");
-        
-        
+
+        const eventCard = heartIcon.closest(".eventcard[data-document-id]");
+        if (eventCard) {
+            const user = firebase.auth().currentUser;
+
+            var documentId = eventCard.getAttribute("data-document-id");
+
+            userDoc = db.collection("users").doc(user.uid);
+
+            if (user) {
+                const userDoc = db.collection("users").doc(user.uid);
+
+                // Update the user's document to remove the favorite event
+                userDoc.update({
+                    favorites: firebase.firestore.FieldValue.arrayRemove(documentId)
+                }).then(() => {
+                    console.log("Event removed from favorites");
+                }).catch(error => {
+                    console.error("Error removing event from favorites:", error);
+                });
+            }
+        }
     }
 });
 
-// function stayFilled(){
 
-// }
+
+
 
 
 
