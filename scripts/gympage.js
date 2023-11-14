53f17b9eb8e7a1ff33fbbe98869610f7382a00c2
Ben Oh <benoh@gmail.com>
2023-11-13 20:31:32 -0800

    implement database to gympage

--- scripts/gympage.js
+++ scripts/gympage.js
@@ -139,3 +139,28 @@ function scheduleStuff() {
     })
 
 }
+
+function displayGymInfo() {
+    let params = new URL( window.location.href ); //get URL of search bar
+    let ID = params.searchParams.get( "docID" ); //get value for key "id"
+    console.log( ID );
+
+    // doublecheck: is your collection called "Reviews" or "reviews"?
+    db.collection( "Gyms" )
+        .doc( ID )
+        .get()
+        .then( doc => {
+            gymName = doc.data().Gym_name;
+            dropIn = doc.data().dropin_info;
+            gymPrice = doc.data().gym_price_info;
+            gymSchedule = doc.data().Schedule;
+            
+            // only populate title, and image
+            document.getElementById( "the-gym-name" ).innerHTML = gymName;
+            document.getElementById("drop-in-info").innerHTML = dropIn;
+            document.getElementById("price-info").innerHTML = gymPrice;
+            document.getElementById("the-gym-schedule").innerHTML = gymSchedule
+
+        } );
+}
+displayGymInfo();
