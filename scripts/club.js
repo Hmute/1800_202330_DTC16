5de74e0aa16dfa3a8339bc69e92b375b24c9597c
Hmute <heraldoabreusound@gmail.com>
2023-11-21 08:23:01 -0800

    this

--- scripts/club.js
+++ scripts/club.js
@@ -4,8 +4,7 @@ function editClubInfo() {
     document.getElementById("club").disabled = false;
 }
 
-function saveClubInfo() {
-
+async function saveClubInfo() {
     console.log('Attempting to save club information...');
 
     // Get the values from the form
@@ -23,50 +22,31 @@ function saveClubInfo() {
         return;
     }
 
-    // Create a storage reference for the image
-    var storageRef = firebase.storage().ref('club_images/' + imageFile.name);
+    try {
+        // Create a storage reference for the image
+        var storageRef = firebase.storage().ref('club_images/' + image.name);
 
-    // Upload the image to Firebase Storage
-    var uploadTask = storageRef.put(imageFile);
+        // Upload the image to Firebase Storage
+        await storageRef.put(image);
 
-    // Listen for state changes, errors, and completion of the upload
-    uploadTask.on('state_changed',
-        function (snapshot) {
-            // Handle progress (optional)
-            var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-            console.log('Upload is ' + progress + '% done');
-        },
-        function (error) {
-            // Handle unsuccessful uploads
-            console.error('Error uploading image: ', error);
-        },
-        function () {
-            // Handle successful uploads on complete
-            console.log('Image uploaded successfully');
+        // Get the download URL of the uploaded image
+        const downloadURL = await storageRef.getDownloadURL();
 
-            // Get the download URL of the uploaded image
-            storageRef.getDownloadURL().then(function (imageUrl) {
-                // Add a new document to the "clubs" collection with the image URL
-                db.collection("clubs").add({
-                    clubName: clubName,
-                    leagueName: leagueName,
-                    players: players,
-                    imageUrl: imageUrl, // Save the image URL in the database
-                })
-                    .then(function (docRef) {
-                        console.log("New club added with ID: ", docRef.id);
-                        // Redirect to the clubs.html page after successfully adding the club
-                        window.location.href = 'clubs.html';
-                    })
-                    .catch(function (error) {
-                        console.error("Error adding new club: ", error);
-                    });
+        // Add a new document to the "clubs" collection with the image URL
+        await db.collection("clubs").add({
+            clubName: clubName,
+            leagueName: leagueName,
+            players: players,
+            imageUrl: downloadURL, // Save the image URL in the database
+        });
 
-                console.log('Club information saved successfully.');
-                document.getElementById('club').disabled = true;
-            });
-        }
-    );
+        console.log('Club information saved successfully.');
+        document.getElementById('club').disabled = true;
+        // Redirect to the clubs.html page after successfully adding the club
+        window.location.href = 'clubs.html';
+    } catch (error) {
+        console.error("Error saving club information: ", error);
+    }
 }
 
 // Call this function when the page loads or whenever you want to refresh the club list
