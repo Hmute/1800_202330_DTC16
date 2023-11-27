6c15b05a5a9886faddc5cec6080ef82d0ffce783
Marc Angelo Arnaldo <arnaldomarcangelo@gmail.com>
2023-11-27 10:46:25 -0800

    Change the sliding bar and the footer buttons spacing

--- scripts/sliding_cards_loader.js
+++ scripts/sliding_cards_loader.js
@@ -1,95 +1,15 @@
-// Import the functions you need from the SDKs you need
-// import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+function copyCard(){
+    var sportList = ["volleyball", "soccer", "tennis", "baseball", "running"]
 
-// Your web app's Firebase configuration
-const firebaseConfig = {
-    apiKey: "AIzaSyAwEwov9jM25EpVg7-4jDfCXbfaznqnKl8",
-    authDomain: "simplyactive.firebaseapp.com",
-    projectId: "simplyactive",
-    storageBucket: "simplyactive.appspot.com",
-    messagingSenderId: "82493903519",
-    appId: "1:82493903519:web:e3409147f56bf9017608aa"
-};
+    sportList.forEach(sport =>{
+        
+        const sportCard = document.getElementById("cardTemp").cloneNode(true)
 
-// Initialize Firebase
-const app = firebase.initializeApp(firebaseConfig);
+        sportCard.querySelector(".copyThis").innerHTML = sport.toUpperCase()
+        sportCard.id = sport
 
-// I added this Richard 
-const db = firebase.firestore();
+        document.getElementById("appendHere").appendChild(sportCard)
 
-
-
-
-$(function () {
-    $(".slidingCardsDiv").load("slidingCards.html");
-});                        
-
-// Path: scripts/sliding_cards.js
-
-// function displayCardsDynamically(collection) {
-//     let cardTemplate = document.getElementById("swipeCardTemplate"); // Retrieve the HTML element with the ID "hikeCardTemplate" and store it in the cardTemplate variable. 
-
-//     db.collection(collection).get()   //the collection called "hikes"
-//         .then(allHikes => {
-//             //var i = 1;  //Optional: if you want to have a unique ID for each hike
-//             allHikes.forEach(doc => { //iterate thru each doc
-//                 var title = doc.data().name;       // get value of the "name" key
-//                 var details = doc.data().details;  // get value of the "details" key
-//                 var hikeCode = doc.data().code;    //get unique ID to each hike to be used for fetching right image
-//                 var hikeLength = doc.data().length; //gets the length field
-//                 var docID = doc.id;
-//                 let newcard = cardTemplate.content.cloneNode(true); // Clone the HTML template to create a new card (newcard) that will be filled with Firestore data.
-
-//                 //update title and text and image
-//                 newcard.querySelector('.card-title').innerHTML = title;
-//                 newcard.querySelector('.card-length').innerHTML = hikeLength + "km";
-//                 newcard.querySelector('.card-text').innerHTML = details;
-//                 newcard.querySelector('.card-image').src = `/images/${hikeCode}.jpg`; //Example: NV01.jpg
-//                 newcard.querySelector('a').href = "eachHike.html?docID=" + docID;
-
-//                 //Optional: give unique ids to all elements for future use
-//                 // newcard.querySelector('.card-title').setAttribute("id", "ctitle" + i);
-//                 // newcard.querySelector('.card-text').setAttribute("id", "ctext" + i);
-//                 // newcard.querySelector('.card-image').setAttribute("id", "cimage" + i);
-
-//                 //attach to gallery, Example: "hikes-go-here"
-//                 document.getElementById(collection + "-go-here").appendChild(newcard);
-
-//                 //i++;   //Optional: iterate variable to serve as unique ID
-//             })
-//         })
-// }
-
-// displayCardsDynamically("hikes");  //input param is the name of the collection
-
-function insertNameFromFirestore() {
-    // Check if the user is logged in:
-    firebase.auth().onAuthStateChanged(user => {
-        if (user) {
-
-            console.log(user.uid); // Let's know who the logged-in user is by logging their UID
-
-            currentUser = db.collection("users").doc(user.uid); // Go to the Firestore document of the user
-
-            currentUser.get().then(userDoc => {
-                // Get the user name
-                var userName = userDoc.data().name;
-
-
-
-                console.log(userName); //just printing  in the database 
-                
-                
-                
-                //$("#name-goes-here").text(userName); // jQuery
-                document.getElementById("name-goes-here").innerText = userName;
-            })
-        } else {
-            console.log("No user is logged in."); // Log a message when no user is logged in
-        }
     })
 }
-
-insertNameFromFirestore();
+copyCard()
